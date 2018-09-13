ad5f81231fad363fa3dae5ab8e12b8bbe769c830
Fernaldi <42663930+Fernaldi@users.noreply.github.com>
2018-09-13 08:48:52 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
 <?php
+	echo "<br> Deret Rekursif<br>";
+	$pembatas = 5;
+  	function rekursif($i = 1){
+  		global $pembatas;
+  		echo "*";
+    	$i++;
+   			if($i <= $pembatas){
+      		rekursif($i);
+    	}
+    		else{
+    		echo"<br>";
+    		$pembatas--;
+    			if ($pembatas !=0){
+    				rekursif();
+    			}
+    		}
+  	}
+ rekursif();
+echo "<br>";
+echo "<br>";
+echo "*======================*<br><br>";
+echo "Deret Piramida";
+echo "<br>";
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
-?>
+ $garis = 1;
+ 	function rekursifb($o= 1){
+ 		global $garis;
+ 		echo "$o";
+ 		$o++;
+ 			if ($o<=$garis){
+ 			rekursifb($o);
+ 	}
+ 	else{
+ 		echo"<br>";
+ 		$garis++;
+ 		if($garis <= 5){
+ 			rekursifb();
+ 		}
+ 	}
+ 	}
+rekursifb();
+ ?>
